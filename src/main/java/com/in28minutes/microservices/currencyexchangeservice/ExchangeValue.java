3b7948d890387654cd9d6f95582ba14f5a432e77
Julian Cabrera <julian.cabreras@gmail.com>
2019-09-22 15:49:48 -0500

    Adding dynamic port

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -3,8 +3,31 @@ package com.in28minutes.microservices.currencyexchangeservice;
 import java.math.BigDecimal;
 
 public class ExchangeValue {
-    private  Long id;
+    private Long id;
     private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    private int port;
+
+    public ExchangeValue() {
+
+    }
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public int getPort() {
+        return port;
+    }
 
     public Long getId() {
         return id;
@@ -20,19 +43,6 @@ public class ExchangeValue {
 
     public BigDecimal getConversionMultiple() {
         return conversionMultiple;
-    }
-
-    private String to;
-    private BigDecimal conversionMultiple;
-    public ExchangeValue(){
-
-    }
-
-    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
-        this.id = id;
-        this.from = from;
-        this.to = to;
-        this.conversionMultiple = conversionMultiple;
     }
 
 
