903cc10266f1513fbcdc49a5aa7bb7c6d4f44868
Julian Cabrera <julian.cabreras@gmail.com>
2019-09-27 19:59:32 -0500

    Implementation of Zipkin

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.core.env.Environment;
@@ -12,7 +14,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private Environment environment;
     @Autowired
@@ -24,7 +26,7 @@ public class CurrencyExchangeController {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from,to);
 
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
-
+        logger.info("{}", exchangeValue);
         return exchangeValue;
 
     }
